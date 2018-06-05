57022df9ada40ffa87abde5fdae390af8dc95009
Vladimir Gasic <vgashic@gmail.com>
2018-06-05 02:11:47 +0200

    partialy done model

---
+++ PhotoSharingApplication/PhotoSharingApplication/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Models {
+	public class Comment {
+		public int CommentID { get; set; }
+		public int PhotoID { get; set; }
+		public string USerName { get; set; }
+		public string Subjet { get; set; }
+		public string Body { get; set; }
+		public DateTime DateCreated { get; set; }
+
+		public virtual Photo Photo { get; set; }
+	}
+}
---
+++ PhotoSharingApplication/PhotoSharingApplication/Models/Photo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Models {
+	public class Photo {
+
+		public int PhotoID { get; set; }
+
+		[Display(Name="Picture")]
+		public string Title { get; set; }
+
+		public byte[] PhotoFile { get; set; }
+
+		public string ImageMimeType { get; set; }
+
+		[DataType(DataType.MultilineText)]
+		public string Description { get; set; }
+
+		public string UserName { get; set; }
+
+		public DateTime CreatedDate { get; set; }
+
+		public virtual List<Comment> Comments { get; set; }
+	}
+}
